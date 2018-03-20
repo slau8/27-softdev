e754ed7d1d1de66e8768d48844ae86d415509cc8
Shannon Lau <shannonlau00@gmail.com>
2018-03-20 08:04:26 -0400

    m

--- script.js
+++ script.js
@@ -5,28 +5,27 @@
 // 2018-03-20
 
 var us_medals = [9, 8, 6];
-var russia_medals = [5, 8, 4];
+var sk_medals = [5, 8, 4];
 var curr = us_medals;
 
 var svg = document.getElementById("svg");
 var heading = document.getElementById("heading");
 var switch = document.getElementById("switch");
 
-var set = function(country, data){
+var switchCountry = function(country, data){
     d3.selectAll("circle").data(data).attr("r", function(d){return 10 * d});
     d3.selectAll("text").data(data).html(function(d){return d});
+    heading.innerHTML = country
 }
 
 var buttonCallBack = function(e){
-    if (curr == US_medal_count){
-    	heading.innerHTML = "South Korea";
-    	curr = SK_medal_count;
+    if (curr == us_medals){
+      switchCountry("South Korea", sk_medals)
     }
-    else if (data == SK_medal_count){
-	heading.innerHTML = "US";
-	data = US_medal_count;
+    else {
+    	switchCountry("United States", us_medals)
     }
       set();
-}
+    }
 
 switch.addEventListener("click", buttonCallBack);
